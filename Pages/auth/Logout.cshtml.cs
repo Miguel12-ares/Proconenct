d46db2d9d8e6efe491c99c5fb0713548252f347a
Miguel12-ares <miguel12.adso@gmail.com>
2025-07-14 09:08:17 -0500

    feat: mejorar la funcionalidad de cierre de sesión, actualizando la forma de redirección y asegurando la eliminación correcta de la cookie jwtToken

--- Pages/auth/Logout.cshtml.cs
+++ Pages/auth/Logout.cshtml.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http; // Added for CookieOptions
+using System; // Added for DateTimeOffset
 
 namespace Proconenct.Pages.auth
 {
@@ -9,9 +11,10 @@ namespace Proconenct.Pages.auth
     {
         public IActionResult OnPost()
         {
+            // Eliminar la cookie jwtToken exactamente igual que en login
             Response.Cookies.Delete("jwtToken");
             HttpContext.User = new System.Security.Claims.ClaimsPrincipal(new System.Security.Claims.ClaimsIdentity());
-            return RedirectToPage("/auth/Login");
+            return Redirect("/auth/Login");
         }
     }
 } 
